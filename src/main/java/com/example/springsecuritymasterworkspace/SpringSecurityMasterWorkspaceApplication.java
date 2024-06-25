e60c1788656278622fc18fdd3344ff8b355e6e3c
KingBlackCow <sgs1159@naver.com>
2024-06-25 23:27:26 +0900

    feat: first init

---
+++ src/main/java/com/example/springsecuritymasterworkspace/SpringSecurityMasterWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritymasterworkspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMasterWorkspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityMasterWorkspaceApplication.class, args);
+    }
+
+}
